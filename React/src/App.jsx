eb7683569c919bcbd188b67ec9d341155c8dcea8
armaanepiic <armaanepiic@gmail.com>
2025-11-12 20:00:41 +0600

    Giving a component multiple state variables

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,10 +1,34 @@
-export default function Button() {
-  function handleClick () {
-    console.log("Clicked");
+import { useState } from "react";
+import { sculptureList } from "./data.js";
+
+export default function App() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleNextClick() {
+    setIndex(index + 1);
   }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
   return (
-    <button onClick={handleClick}>
-      I don't do anything
-    </button>
+    <>
+      <button onClick={handleNextClick}>Next</button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      <img  src={sculpture.url} alt={sculpture.alt} />
+      {showMore && <p>{sculpture.description}</p>}
+    </>
   );
 }
