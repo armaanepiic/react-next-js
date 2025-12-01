5386a874a88e4afb1d193eb667bc5948d8839eb7
armaanepiic <armaanepiic@gmail.com>
2025-12-01 15:18:56 +0600

    1/components-different-visual-states

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,5 +1,9 @@
+import Form from "./components/Form";
+const statuses = ['empty', 'typing', 'submitting', 'success', 'error'];
 export default function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-slate-800 text-gray-50">Primary React + Tailwind Setup</div>
-  )
+    <div>
+      {statuses.map((status) => <Form key={status} status={status} />)}
+    </div>
+  );
 }
